bc8025dab41e41c986c76a1e13a80bc88e1f7079
George Jeremy <ksacck@gmail.com>
2026-09-19 21:37:59 +0700

    неделя 3, новый модуль

---
+++ week3/src/main/java/com/tutorial/ai_challenge/AiChallengeApplicationWeek3.java
@@ -0,0 +1,13 @@
+package com.tutorial.ai_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChallengeApplicationWeek3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChallengeApplicationWeek3.class, args);
+	}
+
+}
