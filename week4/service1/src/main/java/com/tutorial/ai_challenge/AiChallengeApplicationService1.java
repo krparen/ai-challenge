21da0c568c3faa922db7b5d6b11e4f983271a3bd
George Jeremy <ksacck@gmail.com>
2026-09-26 16:50:58 +0700

    неделя 4, заготовка: 2 сервиса на 8080/8081

---
+++ week4/service1/src/main/java/com/tutorial/ai_challenge/AiChallengeApplicationService1.java
@@ -0,0 +1,13 @@
+package com.tutorial.ai_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChallengeApplicationService1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChallengeApplicationService1.class, args);
+	}
+
+}
---
+++ week4/service2/src/main/java/com/tutorial/ai_challenge/AiChallengeApplicationService2.java
@@ -0,0 +1,13 @@
+package com.tutorial.ai_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChallengeApplicationService2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChallengeApplicationService2.class, args);
+	}
+
+}
